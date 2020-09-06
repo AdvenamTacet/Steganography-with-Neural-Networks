4bdac2eb956300de724bce2d8e811fee4daeef28
Tacet <advenam.tacet@zoho.eu>
2020-09-06 05:34:45 +0200

    Basic kernel size manipulation

--- model/e2eCNN.py
+++ model/e2eCNN.py
@@ -16,15 +16,27 @@ class e2eCNN(template.EncoderDecoder):
     image_W = 160
     image_H = 160
 
-    def __init__(self):
+    def __init__(self, bks=3):
+        """
+        Initialize the model.
+        Saves a size of the kernel.
+
+        :param bks: this value will be returned by self.basic_kernel_size and describes kernel size in some layers
+        """
+        self.bks = bks
         super().__init__()
-        self.name = 'e2eCNN'
+
+        self.name = 'e2eCNN_' + str(self.basic_kernel_size())
         try:
             self.load(self.name)
         except Exception as e:
             print(e)
 
+    def basic_kernel_size(self):
+        return self.bks
+
     def gen_encoder_model(self):
+        bks = self.basic_kernel_size()
         cover_image = layers.Input(shape=(self.image_H, self.image_W, 3), name='cover_image')
         hide_image  = layers.Input(shape=(self.image_H, self.image_W, 1), name='hide_image')
 
@@ -35,48 +47,48 @@ class e2eCNN(template.EncoderDecoder):
 
         N = 4
         for i in range(N):
-            conn_layer_in = layers.Concatenate(axis=3,
-                name='encoder_in_' + str(i))([gray_layer, conn_layer])
+            conn_layer_in = layers.Concatenate(axis=3, name='encoder_in_' + str(i))([gray_layer, conn_layer])
 
-            conn_layer = layers.Conv2D(filters=16, kernel_size=3, padding='same', activation='relu',
-                                   name='encoder_color_A_' + str(i))(conn_layer_in)
-            gray_layer = layers.Conv2D(filters=16, kernel_size=3, padding='same', activation='relu',
-                name='encoder_gray_A_' + str(i))(gray_layer)
+            conn_layer = layers.Conv2D(filters=16, kernel_size=bks, padding='same', activation='relu',
+                                       name='encoder_color_A_' + str(i))(conn_layer_in)
+            gray_layer = layers.Conv2D(filters=16, kernel_size=bks, padding='same', activation='relu',
+                                       name='encoder_gray_A_' + str(i))(gray_layer)
 
-            conn_layer = layers.Conv2D(filters=16, kernel_size=(3 if i+1 < N else 1), activation='relu',
-                padding='same', name='encoder_color_B_' + str(i))(conn_layer)
+            conn_layer = layers.Conv2D(filters=16, kernel_size=(bks if i+1 < N else 1), activation='relu',
+                                       padding='same', name='encoder_color_B_' + str(i))(conn_layer)
 
             if i + 1 < N:
-                gray_layer = layers.Conv2D(filters=16, kernel_size=3, padding='same', activation='relu',
-                    name='encoder_gray_B_' + str(i))(gray_layer)
+                gray_layer = layers.Conv2D(filters=16, kernel_size=bks, padding='same', activation='relu',
+                                           name='encoder_gray_B_' + str(i))(gray_layer)
 
         layer_1 = layers.Conv2D(filters=8, kernel_size=1, padding='same', activation='relu',
-                                   name='encoder_l_1')(conn_layer)
+                                name='encoder_l_1')(conn_layer)
 
         out = layers.Conv2D(filters=3, kernel_size=1, padding='same',
-                                   name='encoded_image')(layer_1)
+                            name='encoded_image')(layer_1)
         return tf.keras.Model([cover_image, hide_image], [out], name="encoder_model")
 
     def gen_decoder_model(self):
+        bks = self.basic_kernel_size()
         covered_image = layers.Input(shape=(self.image_H, self.image_W, 3))
 
         layer_0 = layers.Conv2D(filters=3, kernel_size=1, activation='relu',
-            padding='same', name='decoder_l_0')(covered_image)
+                                padding='same', name='decoder_l_0')(covered_image)
 
-        layer_1 = layers.Conv2D(filters=16, kernel_size=3, activation='relu',
-            padding='same', name='decoder_l_1')(layer_0)
+        layer_1 = layers.Conv2D(filters=16, kernel_size=bks, activation='relu',
+                                padding='same', name='decoder_l_1')(layer_0)
 
-        layer_2 = layers.Conv2D(filters=16, kernel_size=3, activation='relu',
-            padding='same', name='decoder_l_2')(layer_1)
+        layer_2 = layers.Conv2D(filters=16, kernel_size=bks, activation='relu',
+                                padding='same', name='decoder_l_2')(layer_1)
 
-        layer_3 = layers.Conv2D(filters=8, kernel_size=3, activation='relu',
-            padding='same', name='decoder_l_3')(layer_2)
+        layer_3 = layers.Conv2D(filters=8, kernel_size=bks, activation='relu',
+                                padding='same', name='decoder_l_3')(layer_2)
 
-        layer_4 = layers.Conv2D(filters=8, kernel_size=3, activation='relu',
-            padding='same', name='decoder_l_4')(layer_3)
+        layer_4 = layers.Conv2D(filters=8, kernel_size=bks, activation='relu',
+                                padding='same', name='decoder_l_4')(layer_3)
 
-        layer_5 = layers.Conv2D(filters=3, kernel_size=3, activation='relu',
-            padding='same', name='decoder_l_5')(layer_4)
+        layer_5 = layers.Conv2D(filters=3, kernel_size=bks, activation='relu',
+                                padding='same', name='decoder_l_5')(layer_4)
 
         out = layers.Conv2D(filters=1, kernel_size=1, padding='same', name='recovered_image')(layer_5)
 
